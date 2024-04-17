06cb5d763d9fd07fa6f09b03f27a307c181c3adf
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-04-17 20:07:00 +0400

    update

--- src/Pages/articles/create/articlecreate.jsx
+++ src/Pages/articles/create/articlecreate.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../../../Components/Header'
 
  function Articlecreate() {
   return (
-    <div>articlecreate</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
--- src/Services/articles.js
+++ src/Services/articles.js
@@ -11,3 +11,25 @@ export const getBlogId = async (id)=>{
     const response = await instanceAxsios.get("posts/" + id);
     return response;
 };
+
+export const crtBlog = async (data) =>{
+    const response = await instanceAxsios({method:"POST", url: "posts", data});
+    return response
+}
+
+export const uptBlog = async (id, data) => {
+    const response = await instanceAxsios({
+        method: "PUT",
+        url: "posts/" + id,
+        data,
+    });
+    return response
+}
+
+export const rmvBlogId = async (id) =>{
+    const response = await instanceAxsios({
+        method: "DELETE",
+        url: "posts/" + id
+    });
+    return response
+}
